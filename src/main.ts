319cd1b82108c28d962da56c50c29b0ca001708f
Joséphine Randet <josephine@randet.com>
2026-09-21 22:42:19 +0200

    feat: tested installing a package with dayjs and found out i was alive for 243765 hours

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import "./todo.css";
 import "./reset.css";
-import "./dateTime.ts";
 import { setDate, startClock } from "./dateTime.ts";
+import dayjs from "dayjs";
+
+const myBirthday = dayjs("1998-12-01");
+const now = dayjs();
+console.log(now.diff(myBirthday, "hour"));
 
 // const todoItem = {
 //   content: "",
